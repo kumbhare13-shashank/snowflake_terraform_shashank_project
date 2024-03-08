bc2824bcd05ca55183258bbba4ac5723f1fc7554
Shashank Kumbhare <162700678+kumbhare13-shashank@users.noreply.github.com>
2024-03-08 20:26:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,8 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB4"
   comment = "Database for Snowflake Terraform demo"
 }
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db4.name
+  name     = "DEMO_SCHEMA_ZOOTA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
